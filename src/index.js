ea6f6ee45981af6b6720160fba20a7d4cb55ffbe
[josevalencia28] <joseluisvalencia654@gmail.com>
2022-08-31 17:04:58 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import {dirname,join} from 'path'
 import { fileURLToPath } from 'url'
 
+import indexRoutes from './routes/index.js'
+
 const app = express()
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -9,8 +11,7 @@ console.log(__dirname, '/viewes')
 
 app.set('views',join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => res.render('index'))
+app.use(indexRoutes)
 
 app.listen(3000)
 console.log('Server is listening on port',3000);    
