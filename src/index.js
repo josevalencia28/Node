8277fb4e19074d96499813622dcf3343efce0149
[josevalencia28] <joseluisvalencia654@gmail.com>
2022-08-31 14:41:18 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import second from 'express'
+import express from 'express'
 
 const app = express()
 
+app.get('/', (req, res) => res.send('hello word'))
+
 app.listen(3000)
-console.log('Server is listening', 3000)
+console.log('Server is listening on port',3000);    
