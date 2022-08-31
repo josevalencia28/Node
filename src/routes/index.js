57bd255de9417fc60ebf36c813e90de64ce6b0de
[josevalencia28] <joseluisvalencia654@gmail.com>
2022-08-31 17:19:39 -0500

    uupdate

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,10 @@ import { Router } from 'express'
 
 const router = Router()
 
-router.get('/', (req, res) => res.render('index', {title: 'first website with Node'}))
+router.get('/', (req, res) => res.render('index', {title: 'Mi Web'}))
 
-router.get('/about', (req, res) => res.render('about'))
+router.get('/about', (req, res) => res.render('about', {title: 'About'}))
 
-router.get('/contact', (req, res) => res.render('contact'))
+router.get('/contact', (req, res) => res.render('contact', {title: 'Contacto'}))
 
 export default router
